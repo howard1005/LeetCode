ddfce58fd7fcacbeacb70faee17faeaeb8c6d0b1
KUB <32010682+howard1005@users.noreply.github.com>
2023-01-26 13:13:35 +0900

    Time: 157 ms (63.47%), Space: 15.5 MB (18.80%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -2,8 +2,6 @@ from heapq import heappush,heappop
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        ans = inf
-        
         edges = [[] for _ in range(n)]
         for f,t,p in flights:
             edges[f].append((t,p))
@@ -14,11 +12,10 @@ class Solution:
         hq = [(0,src,0)] # cost,idx,k
         while hq:
             c,i,kk = heappop(hq)
-            # print(c,i,kk,ps[i][kk])
             if c > ps[i][kk]:
                 continue
             if i == dst:
-                ans = min(ans,c)
+                return c
             if kk > k:
                 continue
             for t,p in edges[i]:
@@ -26,4 +23,4 @@ class Solution:
                     ps[t][kk+1] = c+p
                     heappush(hq,(c+p,t,kk+1))
         
-        return -1 if ans == inf else ans
+        return -1
