7c8d2cefce8f037d44e2b52141c596eb4857f685
KUB <32010682+howard1005@users.noreply.github.com>
2023-02-02 12:36:02 +0900

    Time: 42 ms (55.42%), Space: 13.8 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,6 +1,4 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         d = {c1:c2 for c1,c2 in zip(order,'abcdefghijklmnopqrstuvwxyz')}
-        def conv(s):
-            return ''.join([d[c] for c in s])
-        return words == list(sorted(words, key=conv))
+        return words == list(sorted(words, key=lambda s:''.join([d[c] for c in s])))
