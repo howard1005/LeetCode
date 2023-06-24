9a070029ea00a62fd4b29eeaebb3803bde835497
KUB <32010682+howard1005@users.noreply.github.com>
2023-06-24 19:15:00 +0900

    Time: 584 ms (61.46%), Space: 17.1 MB (50.00%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.py
+++ 0956-tallest-billboard/0956-tallest-billboard.py
@@ -1,38 +1,10 @@
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
-        ans = 0
-        
-        dp2 = {}
-        def dfs2(i,a,b):
-            nonlocal ans
-            if i == len(rods):
-                return 0 if a == b else -inf
+        dp = {0: 0}
+        for x in rods:
+            for d, y in list(dp.items()):
+                dp[d + x] = max(dp.get(x + d, 0), y)
+                dp[abs(d - x)] = max(dp.get(abs(d - x), 0), y + min(d, x))
+        return dp[0]
 
-            k2 = (i,a-b)
-            if k2 in dp2:
-                return dp2[k2]
-            dp2[k2] = -inf
-            
-            ret = dfs2(i+1,a+rods[i],b) # 준것의 높은것을 기준으로 얼마나 높아 졌나
-            if ret != -inf:
-                base = max(a+rods[i],b) # 준것의 높은것
-                dp2[k2] = max(dp2[k2],base + ret - max(a,b)) # 받은것의 높은것을 기준으로 얼마나 높아졌는지 계산
-            
-            
-            ret = dfs2(i+1,a,b+rods[i])
-            if ret != -inf:
-                base = max(a,b+rods[i])
-                dp2[k2] = max(dp2[k2],base + ret - max(a,b))
-            
-            
-            ret = dfs2(i+1,a,b)
-            if ret != -inf:
-                base = max(a,b)
-                dp2[k2] = max(dp2[k2],base + ret - max(a,b))
-            
-            return dp2[k2]
-            
-        ans = dfs2(0,0,0)
-              
-        return ans
                 
