e94a8db25c1051071235c2f7c0090cf983e520f9
KUB <32010682+howard1005@users.noreply.github.com>
2023-08-27 15:00:09 +0900

    Time: 757 ms (35.47%), Space: 58.4 MB (9.55%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        d = {}
+        for stone in stones:
+            d[stone] = 0
+        d[stones[-1]] = 1
+        dp = {}
+        
+        def dfs(n,k):
+            print(n,k)
+            if d[n]:
+                return True
+            
+            if (n,k) in dp:
+                return dp[(n,k)]
+            dp[(n,k)] = False
+            
+            for kk in range(k-1,k+2):
+                if kk <= 0:
+                    continue
+                if n+kk in d:
+                    dp[(n,k)] |= dfs(n+kk,kk)
+            
+            return dp[(n,k)]
+        
+        ans = dfs(0,0)
+        
+        return ans
