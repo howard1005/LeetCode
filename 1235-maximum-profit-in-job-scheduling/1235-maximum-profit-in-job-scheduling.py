6de29495121e4aeb489f11a726186ac91c4ef211
KUB <32010682+howard1005@users.noreply.github.com>
2024-01-06 16:11:09 +0900

    Time: 624 ms (63.85%), Space: 29.2 MB (81.97%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,21 +1,16 @@
+from bisect import bisect_left
+
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         td = sorted([(startTime[i],endTime[i],profit[i]) for i in range(len(startTime))])
         dp = [-1 for _ in range(len(startTime))]
-        def dfs(i,t):
-            if len(startTime)==i:
-                return 0
+        dp[-1] = td[-1][2]
+        
+        for i in range(len(td)-2,-1,-1):
             s,e,p = td[i]
-            ret = 0
-            if s<t:
-                ret = dfs(i+1,t)
-            else:
-                if dp[i] != -1:
-                    return dp[i]
-                dp[i] = 0
-                ret = max(ret,dfs(i+1,t))
-                ret = max(ret,dfs(i+1,e)+p)
-                dp[i] = ret
-            return ret
-        return dfs(0,0)
+            dp[i] = max(dp[i],dp[i+1])
+            j = bisect_left(td,e,i+1,len(td),key=lambda x:x[0])
+            dp[i] = max(dp[i],(dp[j] if j < len(dp) else 0)+p)
+        
+        return dp[0]
         
