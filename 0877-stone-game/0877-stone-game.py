93620375f0d3b5d4f1866d51d48b284f054996d5
KUB <32010682+howard1005@users.noreply.github.com>
2024-03-03 16:00:48 +0900

    Time: 476 ms (14.57%), Space: 44.6 MB (33.59%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,27 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        stones = piles
+        dp = [[-1 for _ in range(len(stones))] for _ in range(len(stones))]
+        
+        def dfs(l,r,n,flag): # allice flag=1, bob flag=-1
+            if l==r:
+                return 0
+            if dp[l][r] != -1:
+                return dp[l][r]
+            if flag == 1:
+                val = 0
+                val = max(val,dfs(l+1,r,n-stones[l],-flag) + flag*(n-stones[l]))
+                val = max(val,dfs(l,r-1,n-stones[r],-flag) + flag*(n-stones[r]))
+                dp[l][r] = val
+            else:
+                val = 1000000000
+                val = min(val,dfs(l+1,r,n-stones[l],-flag) + flag*(n-stones[l]))
+                val = min(val,dfs(l,r-1,n-stones[r],-flag) + flag*(n-stones[r]))
+                dp[l][r] = val
+            return dp[l][r]
+        
+        dfs(0,len(stones)-1,sum(stones),1)
+        
+        print(dp)
+        
+        return dp[0][-1]
