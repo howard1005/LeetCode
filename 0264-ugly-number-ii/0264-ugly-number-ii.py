681df747311f3d8e10c1cba282de905c95fb6263
KUB <32010682+howard1005@users.noreply.github.com>
2024-08-18 15:57:55 +0900

    Time: 1180 ms (5.05%), Space: 16.8 MB (18.38%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        def ugliesUnder(num):
+            l = []
+            for a in range(32):
+                an = 2**a
+                if an>num:break
+                for b in range(32):
+                    bn = 3**b
+                    if an*bn>num:break
+                    for c in range(32):
+                        cn = 5**c
+                        if an*bn*cn>num:break
+                        l.append(an*bn*cn)
+            return l
+            
+        lo,hi = 1,2123366400
+        while lo<=hi:
+            mi = (lo+hi)//2
+            l = ugliesUnder(mi)
+            if len(l) == n:
+                return max(l)
+            elif len(l) < n:
+                lo = mi+1
+            else:
+                hi = mi-1
+
+        return -1
+                
+            
