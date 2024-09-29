8e669d6dd938589a53ffe63d3385cb2c7a15d4d0
KUB <32010682+howard1005@users.noreply.github.com>
2024-09-29 18:47:49 +0900

    Time: 168 ms (29.65%), Space: 33 MB (21.28%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,55 +1,85 @@
+class LN:
+    def __init__(self,i=0,cnt=0,prev=None,next=None):
+        self.i = i
+        self.sd = set()
+        self.prev = prev
+        self.next = next
+
+    def getKey(self):
+        ret = self.sd.pop()
+        self.sd.add(ret)
+        return ret
+
+    def add(self, key):
+        self.sd.add(key)
+
+    def remove(self, key):
+        if key in self.sd:
+            self.sd.remove(key)
+        if self.i and not self.sd:
+            self._del()
+
+    def _del(self):
+        self.prev.next = self.next
+        self.next.prev = self.prev
+
+    def getIncLN(self):
+        if self.next.i == self.i+1:
+            return self.next
+        newLN = LN(self.i+1,0,self,self.next)
+        self.next.prev = newLN
+        self.next = newLN
+        return newLN
+
+    def getDecLN(self):
+        if self.prev.i == self.i-1:
+            return self.prev
+        newLN = LN(self.i-1,0,self.prev,self)
+        self.prev.next = newLN
+        self.prev = newLN
+        return newLN
+        
+
+
 class AllOne:
 
     def __init__(self):
-        self.d = defaultdict(int)
-        self.rd = defaultdict(set)
-        self.maxi = 0
-        self.mini = 0
-
-    def _update(self):
-        for i in range(self.maxi+1,self.maxi-2,-1):
-            if i>0 and self.rd[i]:
-                self.maxi = i
-                break
-        for i in range(self.mini-1,self.mini+2):
-            if i>0 and self.rd[i]:
-                self.mini = i
-                break
+        self.head = LN()
+        self.tail = LN()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.d = defaultdict(lambda: (0,self.head))
 
     def inc(self, key: str) -> None:
-        i = self.d[key]
-        if key in self.rd[i]:
-            self.rd[i].remove(key)
-        self.rd[i+1].add(key)
-        self.d[key] += 1
-        self._update()
+        i,ln = self.d[key]
+        nln = ln.getIncLN()
+        nln.add(key)
+        ln.remove(key)
+        self.d[key] = (i+1,nln)
         
-
     def dec(self, key: str) -> None:
-        i = self.d[key]
-        if key in self.rd[i]:
-            self.rd[i].remove(key)
-        self.rd[i-1].add(key)
-        self.d[key] -= 1
-        self._update()
+        i,ln = self.d[key]
+        if i == 1:
+            ln.remove(key)
+            del self.d[key]
+            return
+        pln = ln.getDecLN()
+        pln.add(key)
+        ln.remove(key)
+        self.d[key] = (i-1,pln)
         
 
     def getMaxKey(self) -> str:
-        if not self.rd[self.maxi]:
+        p = self.tail.prev
+        if p.i == 0:
             return ""
-        sd = self.rd[self.maxi]
-        ret = sd.pop()
-        sd.add(ret)
-        return ret
-        
+        return p.getKey()
 
     def getMinKey(self) -> str:
-        if not self.rd[self.mini]:
+        n = self.head.next
+        if n.i == 0:
             return ""
-        sd = self.rd[self.mini]
-        ret = sd.pop()
-        sd.add(ret)
-        return ret
+        return n.getKey()
 
 
 # Your AllOne object will be instantiated and called as such:
