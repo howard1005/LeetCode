7b6fec21708012175476399ceb0bc445d8d65508
KUB <32010682+howard1005@users.noreply.github.com>
2024-10-09 09:27:44 +0900

    Time: 38 ms (35.01%), Space: 16.7 MB (30.64%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        a,b = 0,0
+
+        for c in s:
+            if c == '(':
+                a += 1
+            else:
+                if a:
+                    a -= 1
+                else:
+                    b += 1
+
+        return a+b
+        
