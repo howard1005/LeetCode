b217e19d0db63fd1e6ecd3597871d3c9e3853142
KUB <32010682+howard1005@users.noreply.github.com>
2025-01-20 13:29:03 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.py
@@ -0,0 +1,26 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        
+        m,n = len(mat),len(mat[0])
+
+        d = {}
+
+        cntd = defaultdict(int)
+
+        for i in range(m):
+            for j in range(n):
+                d[mat[i][j]] = (i,j)
+        
+        for idx,a in enumerate(arr):
+            i,j = d[a]
+            cntd[('r',i)] += 1
+            if cntd[('r',i)] == m:
+                return idx
+            cntd[('c',j)] += 1
+            if cntd[('c',j)] == n:
+                return idx
+        
+        return -1
+
+        
+
