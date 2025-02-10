e3462bf72f1fbcdf80faeb0b3f093ee2c46efdab
KUB <32010682+howard1005@users.noreply.github.com>
2025-02-10 16:40:39 +0900

    Time: 3 ms (30.49%), Space: 17.9 MB (7.88%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        st = []
+        for c in s:
+            if c.isdigit():
+                if st:
+                    st.pop()
+            else:
+                st.append(c)
+        return ''.join(st)
