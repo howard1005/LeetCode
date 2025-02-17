8bb4438c61975bfed782023c9d6d51d4dc69fdf2
KUB <32010682+howard1005@users.noreply.github.com>
2025-02-17 14:07:33 +0900

    Time: 9184 ms (5%), Space: 17.5 MB (99.43%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+
+        def is_ca(j):
+            return True if j+1<len(p) and p[j+1] == '*' else False
+
+        def dfs(i,j):
+            if i >= len(s) and j >= len(p):
+                return True
+            if i < len(s) and j >= len(p):
+                return False
+            if i >= len(s) and j < len(p):
+                while j<len(p) and is_ca(j):
+                    j += 2
+                if j == len(p):
+                    return True
+                return False
+            ca = is_ca(j)
+            if ca:
+                if dfs(i,j+2):
+                    return True
+                for k in range(i,len(s)):
+                    if p[j] == '.':
+                        pass
+                    elif s[k] != p[j]:
+                        break
+                    if dfs(k+1,j+2):
+                        return True
+            else:
+                if p[j] == '.':
+                    pass
+                elif s[i] != p[j]:
+                    return False
+                if dfs(i+1,j+1):
+                    return True
+            return False
+
+        return dfs(0,0)
