3b9cea2e37730353c556375e42e819a0e99676fa
KUB <32010682+howard1005@users.noreply.github.com>
2026-03-29 11:40:58 +0900

    Time: 2 ms (25.73%), Space: 19.5 MB (15.79%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        l1,l2 = list(s1),list(s2)
+        
+        if l1 == l2:
+            return True
+
+        l1[0],l1[2] = l1[2],l1[0]
+        if l1 == l2:
+            return True
+
+        l1[1],l1[3] = l1[3],l1[1]
+        if l1 == l2:
+            return True
+
+        l1[0],l1[2] = l1[2],l1[0]
+        if l1 == l2:
+            return True
+        
+        return False
+        
