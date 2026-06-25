24491f04b477bb4a19c5226a2bd4a441e162e4e3
KUB <32010682+howard1005@users.noreply.github.com>
2026-06-25 13:54:55 +0900

    Time: 62 ms (89.78%), Space: 19.4 MB (64.89%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -0,0 +1,25 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        ans = 0
+
+        sl = SortedList()
+
+        l = []
+        cum = 0
+        for i,n in enumerate(nums):
+            if n == target:
+                cum += 1
+            else:
+                cum -= 1
+            l.append(cum)
+
+        for i,c in enumerate(l):
+            if c > 0:
+                ans += 1
+            j = sl.bisect_left(c)
+            ans += j
+            sl.add(c)
+            
+        return ans
