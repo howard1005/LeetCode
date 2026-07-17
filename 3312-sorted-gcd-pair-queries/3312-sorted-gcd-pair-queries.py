e92a4f231b862807343a5608bb3a7cc970dadf12
KUB <32010682+howard1005@users.noreply.github.com>
2026-07-17 18:34:54 +0900

    Time: 646 ms (37.5%), Space: 41.2 MB (25%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.py
@@ -0,0 +1,44 @@
+from bisect import bisect_left
+from math import gcd
+
+class Solution:
+    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        ans = []
+
+        d = defaultdict(int)
+        for n in nums:
+            d[n] += 1
+            
+        mx = max(nums)
+
+        gl = [0 for _ in range(mx+1)]
+        
+        for g in range(mx,0,-1):
+            multiple_cnt = 0
+
+            for i in range(g, mx + 1, g):
+                multiple_cnt += d[i]
+
+            cnt = multiple_cnt * (multiple_cnt - 1) // 2
+
+            for i in range(g * 2, mx + 1, g):
+                cnt -= gl[i]
+
+            gl[g] = cnt
+        
+        # print(gl)
+
+        gl[0] = -1
+        mni = 0
+        for i in range(1,len(gl)):
+            gl[i] += gl[i-1]
+            if mni == 0 and gl[i]>=0:
+                mni = i
+        
+        # print(gl,mni)
+            
+        for q in queries:
+            i = bisect_left(gl,q,mni,len(gl))
+            ans.append(i)
+
+        return ans
